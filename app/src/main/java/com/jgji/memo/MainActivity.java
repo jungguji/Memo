0dd062d9c5a33ac9d56629abaed0cf88b3ffb77d
jungguji <junggu.ji.dev@gmail.com>
2021-04-28 22:58:24 +0900

    feat: 버튼 기능 추가...
    see : https://www.youtube.com/watch?v=FTvtrTdXBwo&list=PLxBf91VkJLZ_GKDUodT0DeV3B-V8ewj70&index=7

--- app/src/main/java/com/jgji/memo/MainActivity.java
+++ app/src/main/java/com/jgji/memo/MainActivity.java
@@ -5,18 +5,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 
 import java.util.List;
 
-@SuppressLint("StaticFieldLeak")
+@SuppressLint(value = "StaticFieldLeak")
 public class MainActivity extends AppCompatActivity {
 
-    private MemoDatabase db = null;
-    private MemoDAO memoDAO;
+    private MemoDatabase db;
     private List<MemoEntity> memoList;
+    private MemoDAO memoDAO;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,17 +27,58 @@ public class MainActivity extends AppCompatActivity {
         db = MemoDatabase.getInstance(this);
         memoDAO = db.memoDAO();
 
-        final Button button = findViewById(R.id.button_add);
-        final EditText editText = findViewById(R.id.edittext_memo);
-        button.setOnClickListener(v -> {
-            MemoEntity memo = new MemoEntity(-1, editText.getText().toString());
-            memoDAO.insert(memo);
-        });
+//        final Button button = findViewById(R.id.button_add);
+//        final EditText editText = findViewById(R.id.edittext_memo);
+//        button.setOnClickListener(v -> {
+//            final MemoEntity memo = new MemoEntity(-1, editText.getText().toString());
+//            insertMemo(memo);
+//        });
 
-        final RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-
-
-
+//        final RecyclerView recyclerView = findViewById(R.id.recycler_view);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
+
+    public void insertMemo(MemoEntity memo) {
+        final AsyncTask insertTask = new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                db.memoDAO().insert(memo);
+                return null;
+            }
+
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+                getAllMemos();
+            }
+        };
+
+        insertTask.execute();
+    }
+
+    public void getAllMemos() {
+        final AsyncTask getTask = new AsyncTask<Void, Void, Void>() {
+
+            @Override
+            protected Void doInBackground(Void... voids) {
+                memoList = db.memoDAO().getAll();
+                return null;
+            }
+
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+                setRecyclerView(memoList);
+            }
+        };
+
+        getTask.execute();
+    }
+
+    public void setRecyclerView(List<MemoEntity> memoList) {
+//        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+//        recyclerView.setAdapter(new MyAdapter(this, memoList));
+    }
+
+
+
 }
